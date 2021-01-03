bd1009e8bb1cd8c49f0197da2ab30983efae3ea7
Lance Hietpas <lancehietpas@gmail.com>
2021-01-03 08:04:09 -0600

    Update blog-post.js

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -33,9 +33,9 @@ export default class blogPost extends Component {
         <SEO
           title={data.title}
           keywords={[
-            `Rohit Gupta`,
-            `Frontend Developer`,
-            `Developer`,
+            `Matt Sloniker`,
+            `Photographer`,
+            `Drone Photographer`,
             `${data.title}`
           ]}
         />
